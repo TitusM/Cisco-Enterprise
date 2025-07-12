6b55786c9a39d7791161d8867fee036d1b262d9a
tmajeza <tmajeza@gmail.com>
2025-07-12 09:44:02 +0200

    Update get_software_version script

--- IOSXE-Python/get_software_version.py
+++ IOSXE-Python/get_software_version.py
@@ -1,1 +1,21 @@
 
+#A simple python script to get a cisco ios device software version
+import netmiko
+
+ip = '10.0.0.1'
+username = 'cisco'
+password = 'cisco'
+device_type = 'cisco_ios'
+port = '22'
+
+net_connect = netmiko.ConnectHandler(
+    ip = ip,
+    device_type = device_type,
+    username = username,
+    password = password,
+    port = port
+)
+
+show_version = net_connect.send_command('show version')
+
+print(show_version)
