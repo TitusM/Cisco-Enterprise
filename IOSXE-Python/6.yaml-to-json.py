0a24f87ae2197fb7d67af91308985edfb251c605
tmajeza <tmajeza@gmail.com>
2025-07-13 09:20:55 +0200

    working with yaml

---
+++ IOSXE-Python/6.yaml-to-json.py
@@ -0,0 +1,14 @@
+import yaml #install pyyaml
+import json
+
+#load the contents of the file into a variable using the open() built-in method
+yaml_data = open("inventory.yml", "r")
+
+#use the yaml module safe_load() method to load the contents of the file and assign the output to a new variable named data:
+data = yaml.safe_load(yaml_data)
+
+#check the type of data
+print(type(data)) #<class 'dict'>
+
+#convert YAML to JSON.
+print(json.dumps(data, indent=4))
